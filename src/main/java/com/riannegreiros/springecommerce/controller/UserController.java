56136ea3ad4089c58fb88c00c36b35e58fe76db3
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-06-06 18:08:22 -0300

    create token controller

--- src/main/java/com/riannegreiros/springecommerce/controller/UserController.java
+++ src/main/java/com/riannegreiros/springecommerce/controller/UserController.java
@@ -1,13 +1,6 @@
 package com.riannegreiros.springecommerce.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.riannegreiros.springecommerce.entity.Role;
 import com.riannegreiros.springecommerce.entity.User;
-import com.riannegreiros.springecommerce.exception.APIException;
 import com.riannegreiros.springecommerce.service.UserService;
 import com.riannegreiros.springecommerce.utils.AppConstants;
 import com.riannegreiros.springecommerce.utils.GetAllUsersResponse;
@@ -15,18 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
-
-import static com.riannegreiros.springecommerce.utils.JWTConstants.*;
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api/user")
@@ -70,40 +54,5 @@ public class UserController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable(name = "id") UUID id) {
         userService.delete(id);
-    }
-
-    @GetMapping("/token/refresh")
-    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String authorizationHeader = request.getHeader(AUTHORIZATION);
-        if(authorizationHeader != null && authorizationHeader.startsWith(AUTH_TOKEN_TYPE)) {
-            try {
-                String refresh_token = authorizationHeader.substring(AUTH_TOKEN_TYPE.length());
-                Algorithm algorithm = Algorithm.HMAC512(JWT_SECRET);
-                JWTVerifier verifier = JWT.require(algorithm).build();
-                DecodedJWT decodedJWT = verifier.verify(refresh_token);
-                String email = decodedJWT.getSubject();
-                User user = userService.findByEmail(email);
-                String access_token = JWT.create()
-                        .withSubject(user.getEmail())
-                        .withExpiresAt(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION))
-                        .withIssuer(request.getRequestURL().toString())
-                        .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
-                        .sign(Algorithm.HMAC512(JWT_SECRET));
-                Map<String, String> tokens = new HashMap<>();
-                tokens.put("access_token", access_token);
-                tokens.put("refresh_token", refresh_token);
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
-            } catch (Exception e) {
-                response.setHeader("ERROR", e.getMessage());
-                response.setStatus(HttpStatus.FORBIDDEN.value());
-                Map<String, String> error = new HashMap<>();
-                error.put("error_message", e.getMessage());
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), error);
-            }
-        } else {
-            throw new APIException(HttpStatus.BAD_REQUEST, "Refresh token is missing");
-        }
     }
 }
