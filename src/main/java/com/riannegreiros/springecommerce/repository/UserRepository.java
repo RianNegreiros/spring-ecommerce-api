5286dea0a96a72d236a9336abd54d257df6aed51
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-06-06 13:43:35 -0300

    findAllByKeyword

--- src/main/java/com/riannegreiros/springecommerce/repository/UserRepository.java
+++ src/main/java/com/riannegreiros/springecommerce/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.riannegreiros.springecommerce.repository;
 
 import com.riannegreiros.springecommerce.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,7 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     @Query("SELECT u FROM User u WHERE u.email = :email")
     Optional<User> findUserByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.firstName LIKE %?1% OR u.lastName LIKE %?1% OR u.email LIKE %?1%")
+    Page<User> findAllByKeyword(String keyword, Pageable pageable);
 }
--- src/test/java/com/riannegreiros/springecommerce/repositoryTests/UserRepositoryTests.java
+++ src/test/java/com/riannegreiros/springecommerce/repositoryTests/UserRepositoryTests.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,5 +49,14 @@ public class UserRepositoryTests {
     @Test
     public void testFindUserByEmail() {
         assertThat(userRepository.findUserByEmail("any_mail@mail.com")).isNotNull();
+    }
+
+    @Test
+    public void testFindAllByKeyword() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("id").ascending());
+        userRepository.findAllByKeyword("any_mail", pageable);
+
+        assertThat(userRepository.findAllByKeyword("any_mail", pageable)).isNotEmpty();
+        assertThat(userRepository.findAllByKeyword("any_mail", pageable)).first().isEqualTo(user);
     }
 }
