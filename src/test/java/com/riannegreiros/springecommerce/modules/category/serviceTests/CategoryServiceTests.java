29c8734a7de73d90d70e758174a0ec4733f9dcbe
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-06-07 14:57:10 -0300

    test service save category

---
+++ src/test/java/com/riannegreiros/springecommerce/modules/category/serviceTests/CategoryServiceTests.java
@@ -0,0 +1,57 @@
+package com.riannegreiros.springecommerce.modules.category.serviceTests;
+
+import com.riannegreiros.springecommerce.modules.category.entity.Category;
+import com.riannegreiros.springecommerce.modules.category.repository.CategoryRepository;
+import com.riannegreiros.springecommerce.modules.category.service.Impl.CategoryServiceImpl;
+import com.riannegreiros.springecommerce.modules.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoryServiceTests {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+    private CategoryServiceImpl categoryService;
+
+    @BeforeEach
+    void setUp() {
+        categoryService = new CategoryServiceImpl(categoryRepository);
+    }
+
+    @Test
+    public void testSaveCategory() {
+        Category category = new Category("any_category");
+        categoryService.save(category);
+
+        ArgumentCaptor<Category> userArgumentCaptor = ArgumentCaptor.forClass(Category.class);
+
+        verify(categoryRepository).save(userArgumentCaptor.capture());
+
+        Category capturedCategory = userArgumentCaptor.getValue();
+
+        assertThat(capturedCategory).isEqualTo(category);
+    }
+
+    @Test
+    public void testSaveSubCategory() {
+        Category parentCategory = new Category("any_category");
+        Category subCategory = new Category("any_category", parentCategory);
+        categoryService.save(subCategory);
+
+        ArgumentCaptor<Category> userArgumentCaptor = ArgumentCaptor.forClass(Category.class);
+
+        verify(categoryRepository).save(userArgumentCaptor.capture());
+
+        Category capturedSubCategory = userArgumentCaptor.getValue();
+
+        assertThat(capturedSubCategory).isEqualTo(subCategory);
+    }
+}
