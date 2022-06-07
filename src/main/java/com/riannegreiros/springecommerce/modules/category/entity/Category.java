2efc1a854fb25ac5ab8f25eb2e229a5c0925be2b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-06-07 10:02:31 -0300

    create category entity

---
+++ src/main/java/com/riannegreiros/springecommerce/modules/category/entity/Category.java
@@ -0,0 +1,74 @@
+package com.riannegreiros.springecommerce.modules.category.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String alias;
+    private String image;
+
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> children = new HashSet<>();
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+    public Set<Category> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Set<Category> children) {
+        this.children = children;
+    }
+}
