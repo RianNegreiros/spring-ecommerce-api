d1732e91e9faf9e0cca4943c5239e544601a1ba2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-06-13 10:31:32 -0300

    create PATCH endpoint to enable customer

--- src/main/java/com/riannegreiros/springecommerce/modules/customer/controller/CustomerController.java
+++ src/main/java/com/riannegreiros/springecommerce/modules/customer/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
@@ -20,5 +22,11 @@ public class CustomerController {
     public ResponseEntity<Customer> save(@RequestBody Customer customer) {
         Customer savedCustomer = customerService.save(customer);
         return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<String> enable(@PathVariable(name = "id")UUID id) {
+        customerService.enable(id);
+        return new ResponseEntity<>("Customer has been successfully enabled", HttpStatus.OK);
     }
 }
